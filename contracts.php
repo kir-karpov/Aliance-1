f446d225833c1bca3e834e95028375c603fced21
Kirill <kaarpusa2012@gmail.com>
2023-03-29 22:17:49 +0400

    add php shabloni

--- contracts.php
+++ contracts.php
@@ -1,4 +1,5 @@
     <?php 
     $page_title = "Контрактное производство";
-    include_once("header-page.php");?>
+    include_once("header-page.php");
+    include_once("./template-parts/contracts-block.php")?>
     <?php include_once("footer.php");?>
---
+++ template-parts/contracts-block.php
@@ -0,0 +1,86 @@
+<div class="container">
+<div class="cards">
+          <a href="#" class="card">
+            <div class="card-content">
+              <h3 class="card-title">Автомобильная химия</h3>
+              <p class="card-text">
+                Безусловно, сплочённость команды профессионалов позволяет
+                оценить значение форм воздействия.
+              </p>
+            </div>
+            <img
+              src="img/0503577001614868834 1-1.png"
+              alt="Автомобильная химия"
+              class="card-image"
+            />
+          </a>
+          <a href="#" class="card">
+            <div class="card-content">
+              <h3 class="card-title">Бытовая химия</h3>
+              <p class="card-text">
+                А также стремящиеся вытеснить традиционное производство,
+                нанотехнологии функционально разнесены на независимые элементы.
+              </p>
+            </div>
+            <img
+              src="img/0503577001614868834 1.png"
+              alt="Бытовая химия"
+              class="card-image"
+            />
+          </a>
+          <a href="#" class="card">
+            <div class="card-content">
+              <h3 class="card-title">Дезинфицирующие средства</h3>
+              <p class="card-text">
+                Лишь интерактивные прототипы призваны к ответу.
+              </p>
+            </div>
+            <img
+              src="img/5dd4c227c7dc6914837490 1-1.png"
+              alt="Дезинфицирующие средства"
+              class="card-image"
+            />
+          </a>
+          <a href="#" class="card">
+            <div class="card-content">
+              <h3 class="card-title">Пищевые аэрозоли</h3>
+              <p class="card-text">
+                Безусловно, сплочённость команды профессионалов позволяет
+                оценить значение форм воздействия.
+              </p>
+            </div>
+            <img
+              src="img/5dd4c227c7dc6914837490 1.png"
+              alt="Пищевые аэрозоли"
+              class="card-image"
+            />
+          </a>
+          <a href="#" class="card">
+            <div class="card-content">
+              <h3 class="card-title">Косметическая продукция</h3>
+              <p class="card-text">
+                Лишь интерактивные прототипы призваны к ответу.
+              </p>
+            </div>
+            <img
+              src="img/Без имени-2 1-1.png"
+              alt="Косметическая продукция"
+              class="card-image"
+            />
+          </a>
+          <a href="#" class="card">
+            <div class="card-content">
+              <h3 class="card-title">Краски аэрозольные</h3>
+              <p class="card-text">
+                А также стремящиеся вытеснить традиционное производство,
+                нанотехнологии функционально разнесены на независимые элементы.
+              </p>
+            </div>
+            <img
+              src="img/Без имени-2 1.png"
+              alt="Краски аэрозольные"
+              class="card-image"
+            />
+          </a>
+        </div>
+         </div>
